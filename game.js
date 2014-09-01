8426059fd54f22177e49b23d26da357bd2648ce0
candale <candale.andrei@gmail.com>
2014-09-01 22:09:04 +0300

    Initial commit

---
+++ game.js
@@ -0,0 +1,137 @@
+function get_tile(x, y) {
+	var xpath_tile_new ="/html/body/div[@class='container']/div[@class='game-container']"
+		 				   + "/div[@class='tile-container']/div[contains(@class,'tile-position-" + x.toString() + "-" + y.toString() + " tile-new')]"
+		 				   + "/div[@class='tile-inner']/text()";
+
+	var xpath_tile_merged ="/html/body/div[@class='container']/div[@class='game-container']"
+		 				   + "/div[@class='tile-container']/div[contains(@class,'tile-position-" + x.toString() + "-" + y.toString() + " tile-merged')]"
+		 				   + "/div[@class='tile-inner']/text()";	
+
+	var xpath_basic ="/html/body/div[@class='container']/div[@class='game-container']"
+		 				   + "/div[@class='tile-container']/div[contains(@class,'tile-position-" + x.toString() + "-" + y.toString() + "')]"
+		 				   + "/div[@class='tile-inner']/text()";
+	var res;
+	if((res = document.evaluate(xpath_tile_merged, document, null, XPathResult.ANY_TYPE, null).iterateNext()) != null) {
+		return res;
+	}
+
+	if((res = document.evaluate(xpath_tile_new, document, null, XPathResult.ANY_TYPE, null).iterateNext()) != null) {
+		return res;
+	}
+
+	if((res = document.evaluate(xpath_basic, document, null, XPathResult.ANY_TYPE, null).iterateNext()) != null) {
+		return res;
+	}
+
+	return null;
+}
+
+function get_tiles_template() {
+	var game = {};
+	for (i = 1; i <= 4; i++) {
+		game[i] = {}
+		for (j = 1; j <= 4; j++) {
+			game[i][j] = null;
+		}
+	}
+	return game;
+}
+
+function get_tiles() {
+	game = get_tiles_template();
+	for (i = 1; i <= 4; i++) {
+		for (j = 1; j <= 4; j++) {
+			game[j][i] = get_tile(i, j);
+		}
+	}
+	return game;
+}
+
+function stack(direction, tiles) {
+	new_tile = get_tiles_template()
+	var curr_pos;
+	switch(direction) {
+		case "down": {
+			// for each column
+			for(column = 1; column <= 4; column++) {
+				curr_row = 4;
+				// for each row, upwards
+				for(row = 4; row > 0; row--) {
+					if(tiles[row][column] != null) {
+						new_tile[curr_row][column] = tiles[row][column];
+						curr_row --;
+					}
+				}
+			}
+			break;
+		}
+		case "up": {
+			// for each column
+			for(column = 1; column <= 4; column++) {
+				curr_row = 1;
+				// for each row, downwards
+				for(row = 1; row <= 4; row++) {
+					if(tiles[row][column] != null) {
+						new_tile[curr_row][column] = tiles[row][column];
+						curr_row ++;
+					}
+				}
+			}
+			break;
+		}
+		case "left": {
+			// for each row
+			for(row = 1; row <= 4; row++) {
+				curr_column = 1;
+				// for each column, from right to left
+				for(column = 1; column <= 4; column++) {
+					if(tiles[row][column] != null) {
+						new_tile[row][curr_column] = tiles[row][column];
+						curr_column ++;
+					} 
+				}
+			}
+			break;
+		}
+		case "right": {
+			// for each row
+			for(row = 1; row <= 4; row++) {
+				curr_column = 4;
+				// for each column, from right to left
+				for(column = 4; column > 0; column--) {
+					if(tiles[row][column] != null) {
+						new_tile[row][curr_column] = tiles[row][column];
+						curr_column --;
+					}
+				}
+			}
+			break;
+		}
+
+	}
+	return new_tile
+}
+
+function merge(direction, tiles) {
+	new_tile = get_tiles_template()
+	switch(direction) {
+		case "down": {
+			for(column = 1; column <= 4; column ++) {
+				curr_row = 4;
+				for(row = 4; row > 1; row --) {
+					if(tiles[row][column] == null) {
+						continue;
+					}
+					if(tiles[row][column] == tiles[row + 1][column]) {
+						new_tile[curr_row][column] = 2 * tiles[row][column];
+						row ++;
+					} else {
+						new_tile[curr_row][column] = tiles[row][column];
+					}
+					curr_row --;
+				}
+			}
+		}
+	}
+	return new_tile;
+}
